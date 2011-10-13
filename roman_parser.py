4ec09b45a05e923a9bca8ec221c07b81692709e7
David Litvak <dave@dave-netbook.(none)>
2011-10-13 12:41:41 -0300

    new stable version splitted cycle to new method

--- roman_parser.py
+++ roman_parser.py
@@ -14,15 +14,19 @@ class RomanParser(object):
         result = ""
 
         while a_number > 0:
-
-            for r in self.romans:
-                if r.can_parse(a_number):
-                    result, a_number = r.parse(result, a_number)
-                    break
-
-                for p in r.previous:
-                    if p.can_parse(a_number):
-                        result, a_number = p.parse(result, a_number)
-                        break
+           result, a_number = self.parse_number(result, a_number)
 
         return result
+
+    def parse_number(self, result, a_number):
+        for r in self.romans:
+            if r.can_parse(a_number):
+                result, a_number = r.parse(result, a_number)
+                break
+
+            for p in r.previous:
+                if p.can_parse(a_number):
+                    result, a_number = p.parse(result, a_number)
+                    break
+
+        return result, a_number
