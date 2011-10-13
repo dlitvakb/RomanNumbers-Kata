3279f0fdda6e733fb4f845871708b11a0c947c35
David Litvak <dave@dave-netbook.(none)>
2011-10-13 12:53:47 -0300

    moved constants to Roman

--- roman.py
+++ roman.py
@@ -11,3 +11,11 @@ class Roman(object):
         result += self.symbol
         a_number -= self.value
         return result, a_number
+
+MIL = Roman("M", 1000, [Roman("CM", 900, []), Roman("XM", 990, []), Roman("IM", 999, [])])
+QUINIENTOS = Roman("L", 500, [Roman("CL", 400, []), Roman("XL", 490, []), Roman("IL", 499, [])])
+CIEN = Roman("C", 100, [Roman("XC", 90, []), Roman("IC", 99, [])])
+CINCUENTA = Roman("D", 50, [Roman("XD", 40, []), Roman("ID", 49, [])])
+DIEZ = Roman("X",10,[Roman("IX", 9, [])])
+CINCO = Roman("V", 5, [Roman("IV", 4,[])])
+UNO = Roman("I",1,[])
